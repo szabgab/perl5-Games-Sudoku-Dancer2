a79797b1ec5813dcad491525fa37493716112aaf
Gabor Szabo <gabor@szabgab.com>
2017-10-25 14:23:05 +0300

    enable travis

--- lib/Games/Sudoku/Dancer2.pm
+++ lib/Games/Sudoku/Dancer2.pm
@@ -1,9 +1,19 @@
 package Games::Sudoku::Dancer2;
 use Dancer2;
 
+our $VERSION = '0.01';
+
 get '/'=>sub{
     template 'index';
 };
 
 1;
+
+=pod
+
+=head1 NAME
+
+Games::Sudoku::Dancer2 - play Sudoku on the web
+
+=cut
 
